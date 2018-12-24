b3e0aeb1588944802d9731d75026683a7ff6d9a3
daz1761 <dazgti@btinternet.com>
2018-12-24 19:24:28 +0000

    Project commit

---
+++ ConsoleUI/Factory.cs
@@ -0,0 +1,35 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    // this will be used to new-up instances
+    public static class Factory
+    {
+        public static IPerson CreatePerson()
+        {
+            // normally this typically would be more complex
+            return new Person();
+        }
+
+        public static IChore CreateChore()
+        {
+            return new Chore(CreateLogger(), CreateMessageSender());
+        }
+
+        public static ILogger CreateLogger()
+        {
+            return new Logger();
+        }
+
+        public static IMessageSender CreateMessageSender()
+        {
+            //return new Emailer();
+            return new Texter();
+        }
+    }
+}
